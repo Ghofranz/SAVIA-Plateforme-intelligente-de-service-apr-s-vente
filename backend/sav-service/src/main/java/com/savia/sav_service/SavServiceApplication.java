1ce49c0c9a174760683968b0d5e78bff3860ae66
Ghofranz <zouaghighofrane1771@gmail.com>
2026-06-14 17:44:20 +0100

    Initialize SAV service with database and Eureka config

---
+++ backend/sav-service/src/main/java/com/savia/sav_service/SavServiceApplication.java
@@ -0,0 +1,13 @@
+package com.savia.sav_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavServiceApplication.class, args);
+	}
+
+}
